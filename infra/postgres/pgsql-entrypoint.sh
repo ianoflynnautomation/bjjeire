8e7d048b1599953fa01d4e2a4d3e66f57863b877
ianoflynn <ian.oflynn11@gmail.com>
2025-05-06 01:11:38 +0200

    fix dockerfiles

--- infra/postgres/pgsql-entrypoint.sh
+++ infra/postgres/pgsql-entrypoint.sh
@@ -3,12 +3,12 @@ set -e
 
 echo "PostgreSQL wait patroni to start"
 
-# Initialize PostgreSQL data directory (if not already initialized)
+
 if [ ! -d "/var/lib/postgresql/data" ]; then
     mkdir -p /var/lib/postgresql/data
     chown postgres:postgres /var/lib/postgresql/data
     su-exec postgres initdb -D /var/lib/postgresql/data --auth-host=scram-sha-256 --auth-local=scram-sha-256
 fi
 
-# Start Patroni
+echo "Starting Patroni..."
 exec su-exec postgres /venv/bin/patroni /venv/etc/patroni.yml
