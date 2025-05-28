6720e98cdeb67f5f9824171328db37543f5fdadd
ianoflynn <ian.oflynn11@gmail.com>
2025-05-28 19:43:58 +0200

    add integration test folder
    # Conflicts:
    #	Directory.Packages.props

--- src/bjjeire-app/src/features/gyms/components/__tests__/mocks/gym.mock.ts
+++ src/bjjeire-app/src/features/gyms/components/__tests__/mocks/gym.mock.ts
@@ -1,6 +1,15 @@
-import { GymDto, GymStatus, ClassCategory, AffiliationDto, TrialOfferDto } 
-from '../../../../../types/gyms';
-import { LocationDto, SocialMediaDto, GeoCoordinatesDto} from '../../../../../types/common';
+import {
+  GymDto,
+  GymStatus,
+  ClassCategory,
+  AffiliationDto,
+  TrialOfferDto,
+} from '../../../../../types/gyms'
+import {
+  LocationDto,
+  SocialMediaDto,
+  GeoCoordinatesDto,
+} from '../../../../../types/common'
 
 const mockCoordinates: GeoCoordinatesDto = {
   type: 'Point',
@@ -8,36 +17,36 @@ const mockCoordinates: GeoCoordinatesDto = {
   longitude: -6.260273,
   placeName: 'Dublin City Center',
   placeId: 'ChIJL6wn6oAOZ0gRoHEx_fI9oXs',
-};
+}
 
 const mockLocation: LocationDto = {
   address: '123 Main Street, Dublin, D01 A2B3',
   venue: 'Unit 5, Business Park',
   coordinates: mockCoordinates,
-};
+}
 
 const mockSocialMedia: SocialMediaDto = {
   instagram: 'https://instagram.com/testgym',
   facebook: 'https://facebook.com/testgym',
   x: 'https://x.com/testgym',
   youTube: 'https://youtube.com/testgym',
-};
+}
 
 const mockAffiliation: AffiliationDto = {
   name: 'Global BJJ Federation',
   website: 'https://globalbjj.com',
-};
+}
 
 const mockTrialOfferAvailable: TrialOfferDto = {
   isAvailable: true,
   freeClasses: 1,
   freeDays: 0,
   notes: 'Your first class is on us!',
-};
+}
 
 const mockTrialOfferUnavailable: TrialOfferDto = {
   isAvailable: false,
-};
+}
 
 export const MOCK_GYM_FULL: GymDto = {
   id: 'gym-id-123',
@@ -49,13 +58,17 @@ export const MOCK_GYM_FULL: GymDto = {
   trialOffer: mockTrialOfferAvailable,
   location: mockLocation,
   socialMedia: mockSocialMedia,
-  offeredClasses: [ClassCategory.BJJGiAllLevels, ClassCategory.KidsBJJ, ClassCategory.Wrestling],
+  offeredClasses: [
+    ClassCategory.BJJGiAllLevels,
+    ClassCategory.KidsBJJ,
+    ClassCategory.Wrestling,
+  ],
   website: 'https://elitefighters.ie',
   timetableUrl: 'https://elitefighters.ie/timetable',
   imageUrl: 'https://example.com/images/elite_gym.jpg',
   createdOnUtc: new Date().toISOString(),
   updatedOnUtc: new Date().toISOString(),
-};
+}
 
 export const MOCK_GYM_MINIMAL: GymDto = {
   id: '17f3dd8e19ad9a1bf21128a3',
@@ -68,18 +81,18 @@ export const MOCK_GYM_MINIMAL: GymDto = {
     venue: 'Community Hall',
     coordinates: { type: 'Point', latitude: 51.8969, longitude: -8.4863 },
   },
-  socialMedia: { 
+  socialMedia: {
     instagram: '',
     facebook: '',
     x: '',
     youTube: '',
   },
-  offeredClasses: [], 
-};
+  offeredClasses: [],
+}
 
 export const MOCK_GYM_NO_WEBSITE: GymDto = {
   ...MOCK_GYM_FULL,
   id: '17f3dd8e19ad9a1bf21128a3',
   website: undefined,
   timetableUrl: undefined,
-};
+}
