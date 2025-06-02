b4157f05a2b3f367b3a7c382b7bd82fe5d783608
ianoflynn <ian.oflynn11@gmail.com>
2025-06-02 19:57:14 +0200

    clean up dtos

--- src/BjjEire.Application/Common/Validators/PricingModelDtoValidator.cs
+++ src/BjjEire.Application/Common/Validators/PricingModelDtoValidator.cs
@@ -1,68 +1,68 @@
 // BjjEire.Application.Common.Validators.PricingModelDtoValidator
-using System.Collections.Generic;
 using BjjEire.Application.Common.Extensions;
 using BjjEire.Application.Features.BjjEvents.DTOs;
 using BjjEire.Domain.Enums;
 using FluentValidation;
+using System.Collections.Generic;
 
 namespace BjjEire.Application.Common.Validators;
 
-public class PricingModelDtoValidator : AbstractValidator<BjjEventPricingModelDto>
+public class PricingModelDtoValidator : AbstractValidator<PricingModelDto>
 {
-  public PricingModelDtoValidator()
-  {
-    _ = RuleFor(x => x.Type)
-        .ApplyEnumValidator("Pricing Type");
-
-    _ = When(x => x.Type == PricingType.Free, () =>
+    public PricingModelDtoValidator()
     {
-      _ = RuleFor(x => x.Amount)
-              .Equal(0m)
-              .WithName("Amount")
-              .WithMessage(ValidationMessages.MustBeSpecificValueWhen.Message("0", "Pricing Type is Free"))
-              .WithErrorCode("MUST_BE_ZERO_WHEN_FREE");
+        _ = RuleFor(x => x.Type)
+            .ApplyEnumValidator("Pricing Type");
 
-      _ = RuleFor(x => x.Currency!)
-              .ApplyMustBeNullValidator("Currency", "Pricing Type is Free");
+        _ = When(x => x.Type == PricingType.Free, () =>
+        {
+            _ = RuleFor(x => x.Amount)
+                .Equal(0m)
+                .WithName("Amount")
+                .WithMessage(ValidationMessages.MustBeSpecificValueWhen.Message("0", "Pricing Type is Free"))
+                .WithErrorCode("MUST_BE_ZERO_WHEN_FREE");
 
-      _ = RuleFor(x => x.DurationDays)
-              .ApplyMustBeNullValidator("Duration Days", "Pricing Type is Free");
-    });
+            _ = RuleFor(x => x.Currency!)
+                .ApplyMustBeNullValidator("Currency", "Pricing Type is Free");
 
-    _ = When(x => x.Type != PricingType.Free, () =>
-    {
-      _ = RuleFor(x => x.Amount)
-              .GreaterThan(0m)
-              .WithName("Amount")
-              .WithMessage(ValidationMessages.GreaterThan.Message("0"))
-              .WithErrorCode("MUST_BE_POSITIVE_FOR_PAID");
+            _ = RuleFor(x => x.DurationDays)
+                .ApplyMustBeNullValidator("Duration Days", "Pricing Type is Free");
+        });
 
-      _ = RuleFor(x => x.Currency!)
-              .ApplyRequiredValidator("Currency")
-              .ApplyMustBeInSetValidator("Currency", ValidCurrencies, "ISO 4217 currency code (e.g., EUR, USD)");
-    });
+        _ = When(x => x.Type != PricingType.Free, () =>
+        {
+            _ = RuleFor(x => x.Amount)
+                .GreaterThan(0m)
+                .WithName("Amount")
+                .WithMessage(ValidationMessages.GreaterThan.Message("0"))
+                .WithErrorCode("MUST_BE_POSITIVE_FOR_PAID");
 
-    _ = When(x => x.Type == PricingType.FlatRate, () =>
-    {
-      _ = RuleFor(x => x.DurationDays)
-              .ApplyConditionalRequiredValidator("Duration Days", "FlatRate pricing type");
+            _ = RuleFor(x => x.Currency!)
+                .ApplyRequiredValidator("Currency")
+                .ApplyMustBeInSetValidator("Currency", ValidCurrencies, "ISO 4217 currency code (e.g., EUR, USD)");
+        });
 
-      _ = RuleFor(x => x.DurationDays)
-              .GreaterThan(0)
-              .When(dto => dto.DurationDays.HasValue)
-              .WithName("Duration Days")
-              .WithMessage(ValidationMessages.GreaterThan.Message("0"))
-              .WithErrorCode("MUST_BE_POSITIVE_FLAT_RATE_DURATION");
-    });
+        _ = When(x => x.Type == PricingType.FlatRate, () =>
+        {
+            _ = RuleFor(x => x.DurationDays)
+                .ApplyConditionalRequiredValidator("Duration Days", "FlatRate pricing type");
 
-    _ = When(x => x.Type == PricingType.PerSession || x.Type == PricingType.PerDay, () =>
-    {
-      _ = RuleFor(x => x.DurationDays)
-              .ApplyPositiveOrNullValidator("Duration Days", "provided for PerSession or PerDay pricing");
-    });
-  }
+            _ = RuleFor(x => x.DurationDays)
+                .GreaterThan(0)
+                .When(dto => dto.DurationDays.HasValue)
+                .WithName("Duration Days")
+                .WithMessage(ValidationMessages.GreaterThan.Message("0"))
+                .WithErrorCode("MUST_BE_POSITIVE_FLAT_RATE_DURATION");
+        });
 
-  private static readonly HashSet<string> ValidCurrencies = new()
+        _ = When(x => x.Type == PricingType.PerSession || x.Type == PricingType.PerDay, () =>
+        {
+            _ = RuleFor(x => x.DurationDays)
+                .ApplyPositiveOrNullValidator("Duration Days", "provided for PerSession or PerDay pricing");
+        });
+    }
+
+    private static readonly HashSet<string> ValidCurrencies = new()
     {
         "EUR", "USD", "GBP", "JPY", "CAD", "AUD", "CHF", "CNY", "INR", "BRL"
     };
