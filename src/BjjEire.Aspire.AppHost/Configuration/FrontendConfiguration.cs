d53d4038277b70eb04c23999bfb6f300602228d9
ianoflynn <ian.oflynn11@gmail.com>
2025-06-27 12:15:19 +0200

    fix

--- src/BjjEire.Aspire.AppHost/Configuration/FrontendConfiguration.cs
+++ src/BjjEire.Aspire.AppHost/Configuration/FrontendConfiguration.cs
@@ -1,26 +1,25 @@
 
+using BjjEire.Aspire.AppHost.Constants;
+
 namespace BjjEire.Aspire.AppHost.Configuration;
 
 public static class FrontendConfiguration {
 
-    //public static IResourceBuilder<ContainerResource> AddFrontend(
-    public static IResourceBuilder<NodeAppResource> AddFrontend(
-        IDistributedApplicationBuilder builder,
-        //IResourceBuilder<ContainerResource> api) {
-        IResourceBuilder<ProjectResource> api) {
+    public static IResourceBuilder<ContainerResource> AddFrontend(
+    IDistributedApplicationBuilder builder,
+    IResourceBuilder<ContainerResource> api) {
 
         ArgumentNullException.ThrowIfNull(builder);
 
-        // var contextPath = Path.Combine(string.Empty);
-        // var dockerfilePath = Path.Combine(contextPath, string.Empty);
-        // var frontend = builder.AddDockerfile("react", dockerfilePath)
+        var solutionRoot = Path.GetFullPath(Path.Combine(builder.AppHostDirectory, ServiceConstants.BasePath));
+        var contextPath = solutionRoot;
+        var dockerfilePath = Path.Combine(solutionRoot, "src/bjjeire-app/Dockerfile");
 
-        var frontend = builder.AddNpmApp("react", "../../bjjworld-app")
-        .WithReference(api)
+        var frontend = builder.AddDockerfile("frontend", contextPath, dockerfilePath)
         .WaitFor(api)
         .WithExternalHttpEndpoints()
-        // .WithHttpEndpoint(port: ServiceConstants.FrontendHttpPort, targetPort: 80,)
-        // .WithHttpEndpoint(port: ServiceConstants.FrontendHttpsPort, targetPort: 443)
+        .WithHttpEndpoint(port: ServiceConstants.FrontendHttpPort, targetPort: 80)
+        .WithHttpsEndpoint(port: ServiceConstants.FrontendHttpsPort, targetPort: 443, name: "https")
         .WithEnvironment("BROWSER", builder.Configuration["BROWSER"] ?? "none")
         .WithEnvironment("NODE_ENV", builder.Configuration["NODE_ENV"] ?? "production")
         .WithEnvironment("PORT", builder.Configuration["PORT"] ?? "80")
@@ -29,10 +28,7 @@ public static class FrontendConfiguration {
         .WithEnvironment("VITE_APP_API_URL", builder.Configuration["VITE_APP_API_URL"] ?? string.Empty)
         .WithEnvironment("VITE_APP_ENABLE_API_MOCKING", builder.Configuration["VITE_APP_ENABLE_API_MOCKING"] ?? string.Empty)
         .WithEnvironment("VITE_APP_MOCK_API_PORT", builder.Configuration["VITE_APP_MOCK_API_PORT"] ?? string.Empty)
-        .WithEnvironment("VITE_APP_URL", builder.Configuration["VITE_APP_URL"] ?? string.Empty)
-        .WithEnvironment("VITE_APP_PAGE_SIZE", builder.Configuration["VITE_APP_PAGE_SIZE"] ?? string.Empty)
-        //.WithHealthCheck("/health")
-        .PublishAsDockerFile();
+        .WithEnvironment("VITE_APP_URL", builder.Configuration["VITE_APP_URL"] ?? string.Empty);
 
         return frontend;
     }
