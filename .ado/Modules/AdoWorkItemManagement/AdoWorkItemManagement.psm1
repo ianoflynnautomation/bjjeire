01c3c26a07d6820f0e7ea880c82320730b5967ef
ianoflynn <ian.oflynn11@gmail.com>
2025-07-12 00:32:43 +0200

    clean up code

--- .ado/Modules/AdoWorkItemManagement/AdoWorkItemManagement.psm1
+++ .ado/Modules/AdoWorkItemManagement/AdoWorkItemManagement.psm1
@@ -1,185 +1,93 @@
 <#
 .SYNOPSIS
-    A PowerShell module for managing Azure DevOps work items, specifically for test health automation.
+    A module for managing Azure DevOps work items for test health automation.
 .DESCRIPTION
-    Provides resilient, asynchronous functions to query, create, and update work items (Bugs/Tasks) in Azure Boards.
+    Provides functions to query, create, and update Bugs in Azure Boards based on test analysis.
+    Depends on AdoAutomationCore.psm1 for all API interactions.
 .NOTES
-    Version: 2.1
-    Author: Staff SDET
-    Changes in v2.1:
-    - Fixed syntax error in `New-AdoBugForFlakyTest` when building the JSON body.
-    - Ensured the generic `Invoke-AdoRestMethodAsyncWithRetry` helper is present and correct.
+    Version: 1.0
+    Author:
 #>
-
-#region Reusable Functions
+using module ".\AdoAutomationCore.psm1"
 
 function Get-AdoWorkItemsByTag {
-    <#
-    .SYNOPSIS
-        Asynchronously fetches work items from Azure Boards that contain a specific tag.
-    #>
-    param(
-        [Parameter(Mandatory = $true)]
-        [System.Net.Http.HttpClient]$HttpClient,
-        [Parameter(Mandatory = $true)]
-        [string]$Organization,
-        [Parameter(Mandatory = $true)]
-        [string]$Project,
-        [Parameter(Mandatory = $true)]
-        [string]$Tag,
-        [Parameter(Mandatory = $true)]
-        [string]$ApiVersion
-    )
-    $encodedProject = [uri]::EscapeDataString($Project)
-    $wiql = @{
-        query = "SELECT [System.Id], [System.Title], [System.State], [System.Tags] FROM workitems WHERE [System.TeamProject] = @project AND [System.Tags] CONTAINS '$Tag' AND [System.State] <> 'Removed'"
-    } | ConvertTo-Json
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory = $true)][System.Net.Http.HttpClient]$HttpClient,
+    [Parameter(Mandatory = $true)][string]$Organization,
+    [Parameter(Mandatory = $true)][string]$Project,
+    [Parameter(Mandatory = $true)][string]$Tag,
+    [Parameter(Mandatory = $true)][string]$ApiVersion
+  )
+  $wiql = @{
+    query = "SELECT [System.Id], [System.Title], [System.State], [System.Tags] FROM workitems WHERE [System.TeamProject] = @project AND [System.Tags] CONTAINS '$Tag' AND [System.State] <> 'Removed'"
+  } | ConvertTo-Json
 
-    $url = "https://dev.azure.com/$Organization/$encodedProject/_apis/wit/wiql?api-version=$ApiVersion"
-    $wiqlResponse = Invoke-AdoRestMethodAsyncWithRetry -HttpClient $HttpClient -Uri $url -Method Post -Body $wiql
-    $wiqlData = ($wiqlResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult() | ConvertFrom-Json)
+  $url = "https://dev.azure.com/$Organization/$Project/_apis/wit/wiql?api-version=$ApiVersion"
+  $wiqlResponse = Invoke-ResilientRestMethod -HttpClient $HttpClient -Uri $url -Method Post -Body $wiql
 
-    if ($wiqlData.workItems) {
-        $ids = ($wiqlData.workItems.id | ForEach-Object { $_ }) -join ','
-        if ([string]::IsNullOrEmpty($ids)) { return @() }
+  if ($wiqlResponse.workItems) {
+    $ids = ($wiqlResponse.workItems.id) -join ','
+    if ([string]::IsNullOrEmpty($ids)) { return @() }
         
-        $getDetailsUrl = "https://dev.azure.com/$Organization/$encodedProject/_apis/wit/workitems?ids=$ids&`$expand=fields&api-version=$ApiVersion"
-        $detailsResponse = Invoke-AdoRestMethodAsyncWithRetry -HttpClient $HttpClient -Uri $getDetailsUrl
-        return ($detailsResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult() | ConvertFrom-Json).value
-    }
-    return @()
+    $getDetailsUrl = "https://dev.azure.com/$Organization/$Project/_apis/wit/workitems?ids=$ids&`$expand=fields&api-version=$ApiVersion"
+    return (Invoke-ResilientRestMethod -HttpClient $HttpClient -Uri $getDetailsUrl).value
+  }
+  return @()
 }
 
 function New-AdoBugForFlakyTest {
-    <#
-    .SYNOPSIS
-        Creates a new Bug (or Task as a fallback) work item for a flaky test.
-    #>
-    param(
-        [Parameter(Mandatory = $true)]
-        [System.Net.Http.HttpClient]$HttpClient,
-        [Parameter(Mandatory = $true)]
-        [string]$Organization,
-        [Parameter(Mandatory = $true)]
-        [string]$Project,
-        [Parameter(Mandatory = $true)]
-        [string]$TestName,
-        [Parameter(Mandatory = $true)]
-        [string]$Description,
-        [Parameter(Mandatory = $true)]
-        [string]$AreaPath,
-        [Parameter(Mandatory = $true)]
-        [string]$Tags,
-        [Parameter(Mandatory = $true)]
-        [string]$ApiVersion,
-        [string]$AssignedToEmail,
-        [string]$WorkItemType = 'Task'  # NOTE: This should be 'Bug' in enterprize environment. 
-    )
-    $encodedProject = [uri]::EscapeDataString($Project)
-    $url = "https://dev.azure.com/$Organization/$encodedProject/_apis/wit/workitems/`$$WorkItemType`?api-version=$ApiVersion"
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory = $true)][System.Net.Http.HttpClient]$HttpClient,
+    [Parameter(Mandatory = $true)][string]$Organization,
+    [Parameter(Mandatory = $true)][string]$Project,
+    [Parameter(Mandatory = $true)][string]$TestName,
+    [Parameter(Mandatory = $true)][string]$Description,
+    [Parameter(Mandatory = $true)][string]$AreaPath,
+    [Parameter(Mandatory = $true)][string]$Tags,
+    [Parameter(Mandatory = $true)][string]$ApiVersion,
+    [string]$AssignedToEmail,
+    [ValidateSet('Bug', 'Task')][string]$WorkItemType = 'Bug'
+  )
+  $url = "https://dev.azure.com/$Organization/$Project/_apis/wit/workitems/`$$WorkItemType`?api-version=$ApiVersion"
     
-    # Use a list to build the JSON patch payload correctly.
-    $bodyPayload = [System.Collections.Generic.List[object]]::new()
-    $bodyPayload.Add(@{ op = "add"; path = "/fields/System.Title"; value = "[Flaky Test] $TestName" })
-        # TODO: Use Microsoft.VSTS.TCM.ReproSteps when work item is BUG
-    #@{ op = "add"; path = "/fields/Microsoft.VSTS.TCM.ReproSteps"; value = $description },
-    $bodyPayload.Add(@{ op = "add"; path = "/fields/System.Description"; value = $Description })
-    $bodyPayload.Add(@{ op = "add"; path = "/fields/System.AreaPath"; value = $AreaPath })
-    $bodyPayload.Add(@{ op = "add"; path = "/fields/System.Tags"; value = $Tags })
-    $bodyPayload.Add(@{ op = "add"; path = "/fields/Microsoft.VSTS.Common.Priority"; value = 2 })
+  $body = [System.Collections.Generic.List[object]]::new()
+  $body.Add(@{ op = "add"; path = "/fields/System.Title"; value = "[Flaky Test] $TestName" })
+  # Use the correct field based on the Work Item Type
+  $descriptionField = if ($WorkItemType -eq 'Bug') { "Microsoft.VSTS.TCM.ReproSteps" } else { "System.Description" }
+  $body.Add(@{ op = "add"; path = "/fields/$descriptionField"; value = $Description })
+  $body.Add(@{ op = "add"; path = "/fields/System.AreaPath"; value = $AreaPath })
+  $body.Add(@{ op = "add"; path = "/fields/System.Tags"; value = $Tags })
+  $body.Add(@{ op = "add"; path = "/fields/Microsoft.VSTS.Common.Priority"; value = 2 })
 
-    # Add assignment if an email is provided and is not the default "N/A"
-    if (-not [string]::IsNullOrEmpty($AssignedToEmail) -and $AssignedToEmail -ne "N/A") {
-        Write-Host "Attempting to assign bug to '$AssignedToEmail'"
-        $bodyPayload.Add(@{ op = "add"; path = "/fields/System.AssignedTo"; value = $AssignedToEmail })
-    }
+  if ($AssignedToEmail -and $AssignedToEmail -ne "N/A") {
+    $body.Add(@{ op = "add"; path = "/fields/System.AssignedTo"; value = $AssignedToEmail })
+  }
     
-    $body = $bodyPayload | ConvertTo-Json -Depth 5
-
-    $response = Invoke-AdoRestMethodAsyncWithRetry -HttpClient $HttpClient -Uri $url -Method Patch -Body $body -ContentType "application/json-patch+json"
-    return ($response.Content.ReadAsStringAsync().GetAwaiter().GetResult() | ConvertFrom-Json)
+  Invoke-ResilientRestMethod -HttpClient $HttpClient -Uri $url -Method Patch -Body ($body | ConvertTo-Json -Depth 5) -ContentType "application/json-patch+json"
 }
 
-function Update-AdoBugState {
-    <#
-    .SYNOPSIS
-        Updates the state of an existing work item and adds a history comment.
-    #>
-    param(
-        [Parameter(Mandatory = $true)]
-        [System.Net.Http.HttpClient]$HttpClient,
-        [Parameter(Mandatory = $true)]
-        [string]$Organization,
-        [Parameter(Mandatory = $true)]
-        [int]$BugId,
-        [Parameter(Mandatory = $true)]
-        [string]$State,
-        [Parameter(Mandatory = $true)]
-        [string]$Comment,
-        [Parameter(Mandatory = $true)]
-        [string]$ApiVersion,
-        [string]$Tags
-    )
-    $url = "https://dev.azure.com/$Organization/_apis/wit/workitems/$BugId`?api-version=$ApiVersion"
-    $bodyPayload = [System.Collections.Generic.List[object]]::new()
-    $bodyPayload.Add(@{ op = "add"; path = "/fields/System.State"; value = $State })
-    $bodyPayload.Add(@{ op = "add"; path = "/fields/System.History"; value = $Comment })
-    if ($Tags) {
-        $bodyPayload.Add(@{ op = "add"; path = "/fields/System.Tags"; value = $Tags })
-    }
+function Update-AdoWorkItemState {
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory = $true)][System.Net.Http.HttpClient]$HttpClient,
+    [Parameter(Mandatory = $true)][string]$Organization,
+    [Parameter(Mandatory = $true)][int]$WorkItemId,
+    [Parameter(Mandatory = $true)][string]$State,
+    [Parameter(Mandatory = $true)][string]$Comment,
+    [string]$Tags,
+    [Parameter(Mandatory = $true)][string]$ApiVersion
+  )
+  $url = "https://dev.azure.com/$Organization/_apis/wit/workitems/$WorkItemId`?api-version=$ApiVersion"
+  $body = [System.Collections.Generic.List[object]]::new()
+  $body.Add(@{ op = "add"; path = "/fields/System.State"; value = $State })
+  $body.Add(@{ op = "add"; path = "/fields/System.History"; value = $Comment })
+  if ($Tags) {
+    $body.Add(@{ op = "add"; path = "/fields/System.Tags"; value = $Tags })
+  }
     
-    Invoke-AdoRestMethodAsyncWithRetry -HttpClient $HttpClient -Uri $url -Method Patch -Body ($bodyPayload | ConvertTo-Json) -ContentType "application/json-patch+json" | Out-Null
+  Invoke-ResilientRestMethod -HttpClient $HttpClient -Uri $url -Method Patch -Body ($body | ConvertTo-Json) -ContentType "application/json-patch+json"
 }
 
-function Invoke-AdoRestMethodAsyncWithRetry {
-    <#
-    .SYNOPSIS
-        Performs a resilient request to a REST API, supporting different methods and retry logic.
-    #>
-    param(
-        [Parameter(Mandatory = $true)]
-        [System.Net.Http.HttpClient]$HttpClient,
-        [Parameter(Mandatory = $true)]
-        [string]$Uri,
-        [string]$Method = 'GET',
-        [string]$Body,
-        [string]$ContentType = "application/json",
-        [int]$MaxRetries = 3,
-        [int]$RetryDelaySec = 5
-    )
-
-    for ($attempt = 1; $attempt -le $MaxRetries; $attempt++) {
-        try {
-            Write-Host "##vso[task.debug]Attempting to $Method : $Uri (Attempt $attempt of $MaxRetries)"
-            $request = [System.Net.Http.HttpRequestMessage]::new([System.Net.Http.HttpMethod]::$Method, $Uri)
-            if ($Body) {
-                $request.Content = [System.Net.Http.StringContent]::new($Body, [System.Text.Encoding]::UTF8, $ContentType)
-            }
-
-            $response = $HttpClient.SendAsync($request).GetAwaiter().GetResult()
-
-            if ($response.IsSuccessStatusCode) {
-                return $response
-            }
-            else {
-                $errorContent = $response.Content.ReadAsStringAsync().GetAwaiter().GetResult()
-                Write-Warning "API call failed with status code $($response.StatusCode). Response: $errorContent"
-            }
-        }
-        catch {
-            Write-Warning "Exception during API call on attempt $attempt : $($_.Exception.Message)"
-        }
-        if ($attempt -lt $MaxRetries) {
-            $delay = $RetryDelaySec * $attempt
-            Write-Warning "Waiting for $delay seconds before retrying..."
-            Start-Sleep -Seconds $delay
-        }
-        else {
-            throw "Failed to call API '$Uri' after $MaxRetries attempts."
-        }
-    }
-}
-
-#endregion
-
-Export-ModuleMember -Function *
+Export-ModuleMember -Function Get-AdoWorkItemsByTag, New-AdoBugForFlakyTest, Update-AdoWorkItemState
