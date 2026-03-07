b5d947fed929d17e141c33b64f07370b0c6411c5
ianoflynn <ian.oflynn11@gmail.com>
2026-03-07 11:18:47 +0100

    fix

--- src/BjjEire.Infrastructure/AuditInfoProvider.cs
+++ src/BjjEire.Infrastructure/AuditInfoProvider.cs
@@ -2,9 +2,9 @@ using BjjEire.Application.Common.Interfaces;
 
 namespace BjjEire.Infrastructure;
 
-public class AuditInfoProvider(IHttpContextAccessor contextAccessor) : IAuditInfoProvider {
-
-    public string GetCurrentUser() => string.Empty;
+public class AuditInfoProvider(IHttpContextAccessor contextAccessor) : IAuditInfoProvider
+{
 
     public DateTime GetCurrentDateTime() => DateTime.UtcNow;
+    public string GetCurrentUser() => throw new NotImplementedException();
 }
