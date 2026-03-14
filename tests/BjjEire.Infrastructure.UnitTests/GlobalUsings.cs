c5ddd943e324301341ada12cdf27418f42429d0a
ianoflynn <ian.oflynn11@gmail.com>
2026-03-14 09:09:52 +0000

    add infra unit tests to ci

---
+++ tests/BjjEire.Infrastructure.UnitTests/GlobalUsings.cs
@@ -0,0 +1,16 @@
+global using System.Security.Claims;
+
+global using BjjEire.Application.Common.Interfaces;
+global using BjjEire.Domain.Entities;
+global using BjjEire.Infrastructure.Data.Mongo;
+
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.Extensions.Logging;
+
+global using MongoDB.Driver;
+
+global using Moq;
+
+global using Shouldly;
+
+global using Xunit;
