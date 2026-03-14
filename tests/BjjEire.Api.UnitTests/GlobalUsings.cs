1e4b230396cc880bb840beac60ba6689b2494808
Ian O'Flynn <68143624+ianoflynnautomation@users.noreply.github.com>
2026-03-14 14:33:21 +0100

    add api unit tests (#26)

---
+++ tests/BjjEire.Api.UnitTests/GlobalUsings.cs
@@ -0,0 +1,19 @@
+global using System.Reflection;
+global using System.Security.Claims;
+
+global using BjjEire.Application.Common.Exceptions;
+
+global using FluentValidation;
+global using FluentValidation.Results;
+
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+
+global using Moq;
+
+global using Serilog.Events;
+
+global using Shouldly;
+
+global using Xunit;
