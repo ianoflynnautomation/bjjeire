93a6eece4068835501ab267177ad5a656bdf2528
ianoflynn <ian.oflynn11@gmail.com>
2026-03-14 12:01:55 +0100

    add domain unit tests

---
+++ tests/BjjEire.Domain.UnitTests/UniqueIdentifierTests.cs
@@ -0,0 +1,32 @@
+namespace BjjEire.Domain.UnitTests;
+
+[Trait("Category", "Domain")]
+[Trait("Category", "Unit")]
+public sealed class UniqueIdentifierTests
+{
+    [Fact]
+    public void New_ReturnsNonEmptyString()
+    {
+        UniqueIdentifier.New.ShouldNotBeNullOrWhiteSpace();
+    }
+
+    [Fact]
+    public void New_Returns24CharacterString()
+    {
+        UniqueIdentifier.New.Length.ShouldBe(24);
+    }
+
+    [Fact]
+    public void New_ReturnsValidHexString()
+    {
+        var id = UniqueIdentifier.New;
+        id.ShouldAllBe(c => "0123456789abcdef".Contains(c));
+    }
+
+    [Fact]
+    public void New_ReturnsUniqueValueOnEachCall()
+    {
+        var ids = Enumerable.Range(0, 100).Select(_ => UniqueIdentifier.New).ToList();
+        ids.Distinct().Count().ShouldBe(ids.Count);
+    }
+}
