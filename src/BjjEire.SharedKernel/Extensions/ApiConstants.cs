a5a764f940bc6b5017969a811c5affe2fc14dc95
ianoflynn <ian.oflynn11@gmail.com>
2026-04-20 07:55:07 +0200

    chore: optimize ci pipeline job runs

--- src/BjjEire.SharedKernel/Extensions/ApiConstants.cs
+++ src/BjjEire.SharedKernel/Extensions/ApiConstants.cs
@@ -5,6 +5,4 @@ public static class ApiConstants
     public const string ApiGroupNameV1 = "v1";
 
     public const string ApiGroupNameV2 = "v2";
-
-    public const string ApiGroupNameV3 = "v3";
 }
